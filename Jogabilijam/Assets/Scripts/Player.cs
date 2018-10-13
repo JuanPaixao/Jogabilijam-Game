17163cd3f206bb6f2860efe1b660b1af75a6cf39
Juan Paixao <juanpaixao@live.com>
2018-10-13 00:25:28 -0300

    Basic movementation working, not jumping yet

--- Jogabilijam/Assets/Scripts/Player.cs
+++ Jogabilijam/Assets/Scripts/Player.cs
@@ -7,71 +7,95 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
-    private Rigidbody _rb;
+    private CharacterController _characterController;
     private Vector2 _movement;
-    [SerializeField] private Transform _feetPos;
+    [SerializeField] private Transform _feetPos, _upperHeadPos;
     public bool isGrounded, isCrouching, isMoving, canClimb;
-    public float checkGrounded;
+    public float checkGrounded, checkDistanceLedge, fallSpeed;
     public LayerMask ground;
-    private float movHor;
-    private float movVer;
+    public float movHor, movVer;
     private Animator _animator;
+    public string movingDirection;
+    private RaycastHit _hit;
 
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        _characterController = GetComponent<CharacterController>();
+        movingDirection = "right";
     }
     void Update()
     {
         Movement();
+        Debug.DrawRay(_upperHeadPos.position, Vector3.up, Color.yellow, 5f);
+
+
     }
     void FixedUpdate()
     {
-        if (!isCrouching)
-        {
-            _rb.velocity = new Vector2(movHor * _speed * Time.deltaTime, _rb.velocity.y);
-        }
-        if (!canClimb)
-        {
-            _rb.AddForce(Vector2.down * 19.8f);
-        }
-    }
 
+    }
     private void Movement()
     {
-        movHor = Input.GetAxis("Horizontal");
+        movHor = Input.GetAxisRaw("Horizontal");
         movVer = Input.GetAxisRaw("Vertical");
+        transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+        if (!isCrouching)
+        {
+            Vector3 move = new Vector3(movHor * _speed, -fallSpeed, 0);
+            _characterController.Move(move * Time.deltaTime);
+        }
+        else
+        {
+            Vector3 move = new Vector3(movHor * _speed / 2, -fallSpeed, 0);
+            _characterController.Move(move * Time.deltaTime);
+        }
+        isMoving = (movHor == 1 || movHor == -1) ? true : false;
+        if (movHor > 0.1)
+        {
+            movingDirection = "right";
+        }
+        else if (movHor < -0.1)
+        {
+            movingDirection = "left";
+        }
+        if (Physics.Raycast(_upperHeadPos.position, Vector3.up, out _hit, 0.5f, ground))
+        {
+            Debug.Log(_hit.transform.name);
+        }
 
-        isMoving = (movHor > 0.1f || movHor < 0.1f) ? true : false;
+        _characterController.transform.eulerAngles = (movingDirection == "right") ? new Vector3(_characterController.transform.eulerAngles.x, -220, _characterController.transform.eulerAngles.z) :
+        new Vector3(_characterController.transform.eulerAngles.x, -440, 0);
 
-        isGrounded = (Physics.Raycast(this.transform.position, Vector3.down, checkGrounded, ground));
+
+        isGrounded = (Physics.Raycast(_feetPos.position, Vector3.down, checkGrounded, ground));
 
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-            _rb.velocity = Vector3.up * _jumpForce;
+            //jump  
         }
 
-        isCrouching = (isGrounded && movVer < -0.5f) ? true : false;
-
+        isCrouching = ((isGrounded && movVer < -0.01f) || Physics.Raycast(_upperHeadPos.position, Vector3.up, out _hit, 0.5f, ground)) ? true : false;
         if (isCrouching)
         {
-            _rb.velocity = Vector3.zero;
+            //set character velocity to 0;
         }
+
+        if (movVer < -0.25f)
+        {
+            canClimb = false;
+        }
+
     }
     public void FinishedClimb()
     {
         _animator.SetBool("isClimbing", false);
     }
-    void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Wall"))
-        {
-            canClimb = true;
-            _rb.velocity = Vector3.zero;
-            _rb.AddForce(Vector2.up);
-        }
+        canClimb = true;
+        //set character velocity to 0;
     }
-    void OnCollisionExit(Collision other)
+    private void OnTriggerExit(Collider other)
     {
         canClimb = false;
     }
---
+++ Jogabilijam/Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+
+    private Animator _animator;
+    [SerializeField] private Player _player;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        _animator.SetFloat("HorizontalMovement", Mathf.Abs(_player.movHor));
+        _animator.SetBool("isGrounded", _player.isGrounded);
+        _animator.SetBool("isCrouching", _player.isCrouching);
+        _animator.SetFloat("VerticalMovement", Mathf.Abs(_player.movVer));
+    }
+}
